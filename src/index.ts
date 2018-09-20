477e2ddb9fb39de86f8046d8bfbd8b72c7bcd9b5
hojin.joseph.choi <escape3rd@gmail.com>
2018-09-20 17:13:06 +0900

    # 10 && 11 Validating Block Structure

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import * as CryptoJS from 'crypto-js';
+import * as CryptoJS from "crypto-js";
 
 class Block {
   public index: number;
@@ -13,7 +13,7 @@ class Block {
     previousHash: string,
     data: string,
     timestamp: number
-  ){
+  ) {
     this.index = index;
     this.hash = hash;
     this.previousHash = previousHash;
@@ -22,16 +22,26 @@ class Block {
   }
 
   static calculateBlockHash = (
-    index: number, 
-    previousHash: string, 
-    timestamp: number, 
+    index: number,
+    previousHash: string,
+    timestamp: number,
     data: string
   ): string => {
     return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
   };
+
+  static validateStructure = (aBlock: Block): boolean => {
+    return (
+      typeof aBlock.index === "number" &&
+      typeof aBlock.hash === "string" &&
+      typeof aBlock.previousHash === "string" &&
+      typeof aBlock.timestamp === "number" &&
+      typeof aBlock.data === "string"
+    );
+  };
 }
 
-const genesisBlock: Block = new Block(0, '19191919919', '', 'Hello', 123456);
+const genesisBlock: Block = new Block(0, "19191919919", "", "Hello", 123456);
 
 let blockchains: Block[] = [genesisBlock];
 
@@ -46,21 +56,47 @@ const createNewBlock = (data: string): Block => {
   const newIndex: number = previousBlock.index + 1;
   const newTimestamp: number = getNewTimeStamp();
   const newHash: string = Block.calculateBlockHash(
-    newIndex, 
-    previousBlock.hash, 
-    newTimestamp, 
+    newIndex,
+    previousBlock.hash,
+    newTimestamp,
     data
   );
   const newBlock: Block = new Block(
-    newIndex, 
-    newHash, 
-    previousBlock.hash, 
-    data, 
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
     newTimestamp
   );
   return newBlock;
-}
+};
 
-console.log(createNewBlock('hello'), createNewBlock('bye bye'));
+const getHashForBlock = (aBlock: Block): string =>
+  Block.calculateBlockHash(
+    aBlock.index,
+    aBlock.previousHash,
+    aBlock.timestamp,
+    aBlock.data
+  );
+
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+  if (!Block.validateStructure(candidateBlock)) {
+    return false;
+  } else if (previousBlock.index + 1 !== candidateBlock.index) {
+    return false;
+  } else if (previousBlock.hash !== candidateBlock.previousHash) {
+    return false;
+  } else if (getHashForBlock(candidateBlock) !== candidateBlock.hash) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const addBlock = (candidateBlock: Block): void => {
+  if (isBlockValid(candidateBlock, getLatestBlock())) {
+    getBlockchains().push(candidateBlock);
+  }
+};
 
 export {};
--- textbook.md
+++ textbook.md
@@ -3,24 +3,29 @@
 Learning Typescript by making a Blockchain with it.
 
 # 1 Introduction and What are we building
-- typescript: javascript의 superset
-- typescript를 컴파일하면 javascript로 변함
-- 큰 프로젝트, 팀으로 일한다거나 버그를 최소화하고 싶을 때 javascript의 덜 엄격한 부분이 단점으로 작용한다.
+
+- typescript: javascript 의 superset
+- typescript 를 컴파일하면 javascript 로 변함
+- 큰 프로젝트, 팀으로 일한다거나 버그를 최소화하고 싶을 때 javascript 의 덜 엄격한 부분이 단점으로 작용한다.
 
 - project 시작
-~~~
+
+```
 $ yarn init
-~~~
+```
 
 # 2 Setting Typescript Up
-- typescript 설치
-~~~
-$ yarn add typescript
-~~~
 
-- tsconfig.json: Typescript에게 어떻게 javascript로 변환하는지 알려주면서 옵션을 줄 수 있다.
+- typescript 설치
+
+```
+$ yarn add typescript
+```
+
+- tsconfig.json: Typescript 에게 어떻게 javascript 로 변환하는지 알려주면서 옵션을 줄 수 있다.
 - tsconfig.json 초기옵션
-~~~
+
+```
 {
   "compilerOptions": {
     "module": "commonjs",
@@ -34,54 +39,68 @@ $ yarn add typescript
     "node_modules"
   ]
 }
-~~~
+```
 
 - tsc (typscript compiler)를 사용한 index.ts -> index.js, index.js.map 생성
-~~~
+
+```
 $ tsc
 또는
 $ ./node_modules/.bin/tsc
-~~~
+```
 
 - yarn start 시 ts 컴파일 + js 실행되도록 package.json 수정
-~~~
+
+```
 "scripts": {
   "start": "node index.js",
   "prestart": "tsc"
 }
-~~~
-
+```
 
 # 3 First steps with Typescript
 
-
 # 4 Types in Typescript
-- typescript파일이 수정될 때마다 컴파일 할 수 있도록 함
-~~~
-$ yarn add tsc-watch --dev
-~~~
 
+- typescript 파일이 수정될 때마다 컴파일 할 수 있도록 함
+
+```
+$ yarn add tsc-watch --dev
+```
 
 # 5 Interfaces on Typescript
+
 - interface -> model 정의할 때 유용
 
-
 # 6 Classes on Typescript part One
-- interface는 js로 컴파일하지 않는다.
-- class는 js로 컴파일 한다.
 
+- interface 는 js 로 컴파일하지 않는다.
+- class 는 js 로 컴파일 한다.
 
 # 7 & 8 & 9 Blockchain Creating a Block
-- block의 속성, 타입
-- function의 리턴타입 등등을 확인할 수 있다.
+
+- block 의 속성, 타입
+- function 의 리턴타입 등등을 확인할 수 있다.
 
 - crypto-js 설치 (hash 생성)
-~~~
-$ yarn add crypto-js
-~~~
 
-- typescript에서 import하는 과정은 약간 다르다...
+```
+$ yarn add crypto-js
+```
+
+- typescript 에서 import 하는 과정은 약간 다르다...
 - 기본적인 import 방법
-~~~
+
+```
 import * as CryptoJS from 'crypto-js';
-~~~
+```
+
+# 10 && 11 Validating Block Structure
+
+- vscode prettier + ESLint 연동
+
+```
+$ yarn add --dev prettier-eslint
+```
+
+- [https://velopert.com/3671](https://velopert.com/3671)
